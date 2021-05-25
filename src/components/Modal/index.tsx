6685cd43b39f74d74eca3d0be5efbf0394f2624c
luucaspaixao <luucaspaixao1997@yahoo.com>
2021-05-25 13:54:32 -0300

    Desafio 04 concluído

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState, ReactNode, useRef } from 'react';
+import ReactModal from 'react-modal';
+
+interface ModalProps {
+  isOpen: boolean,
+  setIsOpen: () => void;
+  children: ReactNode
+}
+
+interface Ref {
+  current: boolean
+}
+
+export default function Modal ({ isOpen, children, setIsOpen }: ModalProps) {
+  const [modalStatus, setModalStatus] = useState(false)
+
+  const prevModalStatusRef = useRef(modalStatus)
+
+  useEffect(() => {
+    prevModalStatusRef.current = modalStatus
+    if (prevModalStatusRef.current !== isOpen) {
+      setModalStatus(isOpen)
+    }
+  }, [isOpen])
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+}
